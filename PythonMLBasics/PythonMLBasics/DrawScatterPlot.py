ca8921f4a69bf819226fb342ad4930275092e468
Ben <ben@ben-rush.net>
2018-09-21 15:17:41 -0500

    Poop

--- PythonMLBasics/PythonMLBasics/DrawScatterPlot.py
+++ PythonMLBasics/PythonMLBasics/DrawScatterPlot.py
@@ -7,15 +7,25 @@ N = 1000
 xRange = np.arange(0, 100, 1)
 random_y = [np.random.normal(x, 15) * 9 for x in xRange]
 
-print(random_y[9])
-
 for i in range(0, len(random_y)):
     if random_y[i] < 0:
         random_y[i] = abs(random_y[i])
 
-plt.scatter(x = xRange, y = random_y)
-plt.plot(xRange, xRange * 2, 'r')
+slope = 2
+for i in range(0, 190):
+    print(i)
+    sum = 0
+    for x in range(0, len(random_y)):
+        err = ((x * slope) - random_y[x]) * x
+        sum = sum + err
+    error = sum / len(random_y)
+    slope = slope - (.00001 * error)
 
-plt.xlabel("Days")
-plt.ylabel("# of Apples")
-plt.show()
+    plt.scatter(x = xRange, y = random_y)
+    plt.plot(xRange, xRange * slope, 'r')
+
+    plt.xlabel("Days")
+    plt.ylabel("# of Apples")
+    
+    plt.savefig(str(i) + "_fig.jpeg")
+    plt.close()
