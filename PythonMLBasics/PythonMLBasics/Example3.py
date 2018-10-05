ad9da10ba4616c6825f75c7c88d494e3085d2cdc
Ben <ben@ben-rush.net>
2018-10-05 15:27:26 -0500

    Fart

--- PythonMLBasics/PythonMLBasics/Example3.py
+++ PythonMLBasics/PythonMLBasics/Example3.py
@@ -18,30 +18,29 @@ k = 0
 lastAccuracy = 0
 while True:
 
-    bSum = 0
     treeTypeAMapped = treeTypeA * m + b
     treeTypeAMapped = np.piecewise(treeTypeAMapped, [treeTypeAMapped < 0, treeTypeAMapped >= 0], [-1, 1])
 
     treeTypeBMapped = treeTypeB * m + b
     treeTypeBMapped = np.piecewise(treeTypeBMapped, [treeTypeBMapped < 0, treeTypeBMapped >= 0], [-1, 1])
-
-    #treeTypeAMapped = np.array(list(map(convertTo0And1, treeTypeA * m + b)))
-    #treeTypeBMapped = np.array(list(map(convertTo0And1, treeTypeB * m + b)))
+        
+    mSum = np.sum((treeTypeAMapped - -1) * treeTypeA) + np.sum((treeTypeBMapped - 1) * treeTypeB)
+    bSum = np.sum(treeTypeAMapped - -1) + np.sum(treeTypeBMapped - 1)
 
     #mSum = np.sum(((treeTypeA * m + b) - -1) * treeTypeA) + np.sum(((treeTypeB * m + b) - 1) * treeTypeB)
     #bSum = np.sum((treeTypeA * m + b) - -1 ) + np.sum((treeTypeB * m + b) - 1 )
-    mSum = np.sum((treeTypeAMapped - -1) * treeTypeAMapped) + np.sum((treeTypeBMapped - 1) * treeTypeBMapped)
-    bSum = np.sum(treeTypeAMapped - -1) + np.sum(treeTypeBMapped - 1)
 
     mErr = 2 * (mSum / (len(treeTypeA) + len(treeTypeB)))
     bErr = 2 * (bSum / (len(treeTypeA) + len(treeTypeB)))
 
-    m = (m - mErr * .001)
-    b = (b - bErr * .001)
+    m = (m - mErr * .00001)
+    b = (b - bErr * .00001)
 
     accuracy = Accuracy(treeTypeA, treeTypeB, m, b)
 
-    if k % 1000 == 0:
+    if k % 5000 == 0:
+        print()
+        print(str(m) + "x + " + str(b))
         print("Iteration " + str(k) + ": " + str(accuracy * 100.0))
         lastAccuracy = accuracy
 
